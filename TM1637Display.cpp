9c86c5b1aea0e0d2ca751b9a231a2574211e7ade
shanbor <andrey.shurmin@yandex.ru>
2015-05-18 23:51:03 +0300

    Update TM1637Display.cpp

--- TM1637Display.cpp
+++ TM1637Display.cpp
@@ -13,6 +13,7 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
 
 extern "C" {
   #include <stdlib.h>
@@ -20,69 +21,41 @@ extern "C" {
   #include <inttypes.h>
 }
 
-#include <TM1637Display.h>
+#include <TM1637.h>
 #include <Arduino.h>
 
-#define TM1637_I2C_COMM1    0x40
-#define TM1637_I2C_COMM2    0xC0
-#define TM1637_I2C_COMM3    0x80
 
-//
-//      A
-//     ---
-//  F |   | B
-//     -G-
-//  E |   | C
-//     ---
-//      D
-const uint8_t digitToSegment[] = {
- // XGFEDCBA
-  0b00111111,    // 0
-  0b00000110,    // 1
-  0b01011011,    // 2
-  0b01001111,    // 3
-  0b01100110,    // 4
-  0b01101101,    // 5
-  0b01111101,    // 6
-  0b00000111,    // 7
-  0b01111111,    // 8
-  0b01101111,    // 9
-  0b01110111,    // A
-  0b01111100,    // B
-  0b00111001,    // C
-  0b01000111,    // D
-  0b01111001,    // E
-  0b01110001     // F
-  };
- 
-	
-TM1637Display::TM1637Display(uint8_t pinClk, uint8_t pinDIO)
+TM1637::TM1637(uint8_t pinClk, uint8_t pinDIO)
 {
-	// Copy the pin numbers
-	m_pinClk = pinClk;
-	m_pinDIO = pinDIO;
-	
-	// Set colon off by default? Or change the constructor?
-	m_colon = false;
-	
-	// Set the pin direction and default value.
-	// Both pins are set as inputs, allowing the pull-up resistors to pull them up
-    pinMode(m_pinClk, INPUT);
-    pinMode(m_pinDIO,INPUT);
-	digitalWrite(m_pinClk, LOW);
-	digitalWrite(m_pinDIO, LOW);
+      // Copy the pin numbers
+      m_pinClk = pinClk;
+      m_pinDIO = pinDIO;
+      	
+      // Set colon off by default? Or change the constructor?
+      m_colon = false;
+      	
+      // Set the pin direction and default value.
+      // Both pins are set as inputs, allowing the pull-up resistors to pull them up
+      pinMode(m_pinClk, INPUT);
+      pinMode(m_pinDIO,INPUT);
+      digitalWrite(m_pinClk, LOW);
+      digitalWrite(m_pinDIO, LOW);
 }
 
-void TM1637Display::setBrightness(uint8_t brightness)
+
+void TM1637::setBrightness(uint8_t brightness)
 {
-	m_brightness = brightness;
+  m_brightness = brightness;
 }
 
-void TM1637Display::setColon(bool colon)
+
+void TM1637::setColon(bool colon)
 {
-	m_colon = colon;
+  m_colon = colon;
 }
-void TM1637Display::setSegments(const uint8_t segments[], uint8_t length, uint8_t pos)
+
+
+void TM1637::setSegments(const uint8_t segments[], uint8_t length, uint8_t pos)
 {
     // Write COMM1
 	start();
@@ -95,7 +68,7 @@ void TM1637Display::setSegments(const uint8_t segments[], uint8_t length, uint8_
 	
 	// Write the data bytes
 	for (uint8_t k=0; k < length; k++) 
-	  writeByte(segments[k]);
+	 writeByte(segments[k]);
 	  
 	stop();
 
@@ -105,47 +78,50 @@ void TM1637Display::setSegments(const uint8_t segments[], uint8_t length, uint8_
 	stop();
 }
  
-void TM1637Display::showNumberDec(int num, bool leading_zero, uint8_t length, uint8_t pos)
+
+void TM1637::showNumberDec(int num, bool leading_zero, uint8_t length, uint8_t pos)
 {
 	uint8_t digits[4];
 	const static int divisors[] = { 1, 10, 100, 1000 };
 	bool leading = true;
 	
 	for(int8_t k = 0; k < 4; k++) {
-	    int divisor = divisors[4 - 1 - k];
-		int d = num / divisor;
-		
-		if (d == 0) {
+	  int divisor = divisors[4 - 1 - k];
+	  int d = num / divisor;
+	  
+          if (d == 0) {
 		  if (leading_zero || !leading || (k == 3))
 		    digits[k] = encodeDigit(d);
-	      else
+	          else
 		    digits[k] = 0;
-		}
+		  }
 		else {
-			digits[k] = encodeDigit(d);
-			num -= d * divisor;
-			leading = false;
+		  digits[k] = encodeDigit(d);
+		  num -= d * divisor;
+		  leading = false;
 		}
-	}
-	//As seen on http://mbed.org/users/seeed/code/DigitDisplay/file/d3173c8bfd48/DigitDisplay.cpp lines 147, 148
-	if(m_colon)
-		digits[COLON_POSITION] |= 0x80;
+	  }
+	
+	if(m_colon)  digits[COLON_POSITION] |= 0x80;
 	
 	setSegments(digits + (4 - length), length, pos);
 }
 
-void TM1637Display::bitDelay()
+
+void TM1637::bitDelay()
 {
 	delayMicroseconds(50);
 }
    
-void TM1637Display::start()
+
+void TM1637::start()
 {
   pinMode(m_pinDIO, OUTPUT);
   bitDelay();
 }
    
-void TM1637Display::stop()
+
+void TM1637::stop()
 {
 	pinMode(m_pinDIO, OUTPUT);
 	bitDelay();
@@ -155,7 +131,7 @@ void TM1637Display::stop()
 	bitDelay();
 }
   
-bool TM1637Display::writeByte(uint8_t b)
+bool TM1637::writeByte(uint8_t b)
 {
   uint8_t data = b;
 
@@ -165,7 +141,7 @@ bool TM1637Display::writeByte(uint8_t b)
     pinMode(m_pinClk, OUTPUT);
     bitDelay();
     
-	// Set data bit
+    // Set data bit
     if (data & 0x01)
       pinMode(m_pinDIO, INPUT);
     else
@@ -173,7 +149,7 @@ bool TM1637Display::writeByte(uint8_t b)
     
     bitDelay();
 	
-	// CLK high
+    // CLK high
     pinMode(m_pinClk, INPUT);
     bitDelay();
     data = data >> 1;
@@ -191,19 +167,21 @@ bool TM1637Display::writeByte(uint8_t b)
   uint8_t ack = digitalRead(m_pinDIO);
   if (ack == 0)
     pinMode(m_pinDIO, OUTPUT);
-	
-	
   bitDelay();
   pinMode(m_pinClk, OUTPUT);
   bitDelay();
-  
   return ack;
 }
 
-uint8_t TM1637Display::encodeDigit(uint8_t digit)
+
+uint8_t TM1637::encodeDigit(uint8_t digit)
 {
 	return digitToSegment[digit & 0x0f];
 }
 
-   
+void TM1637::clear(void)
+{
+  uint8_t data[] = { 0,0,0,0 };
+  setSegments(data);
 
+}
